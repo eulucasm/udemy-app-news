8a44acdb623236aa49f519229affd515f635cde5
eulucasm <lucaspoletis@gmail.com>
2022-02-17 12:39:03 -0300

    Configuração Retrofit

---
+++ app/src/main/java/daniellopes/io/newsappstarter/network/NewsApi.kt
@@ -0,0 +1,24 @@
+package daniellopes.io.newsappstarter.network
+
+import daniellopes.io.newsappstarter.model.NewsResponse
+import daniellopes.io.newsappstarter.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+   @GET("/v2/top-headlines")
+   suspend fun getBreakingNews(
+      @Query("country") countryCode: String = "br",
+      @Query("page") pageNumber: Int = 1,
+      @Query("apiKey") apiKey: String = API_KEY
+   ): Response<NewsResponse>
+
+   @GET("/v2/everything")
+   suspend fun searchNews(
+      @Query("q") searchQuery: String,
+      @Query("page") pageNumber: Int = 1,
+      @Query("apiKey") apiKey: String = API_KEY
+   ): Response<NewsResponse>
+}
---
+++ app/src/main/java/daniellopes/io/newsappstarter/util/Constants.kt
@@ -0,0 +1,9 @@
+package daniellopes.io.newsappstarter.util
+
+class Constants {
+
+   companion object {
+      const val API_KEY = "3420c7a024a84470a573e0fbf398e4fc"
+      const val BASE_URL = "https://newsapi.org"
+   }
+}
